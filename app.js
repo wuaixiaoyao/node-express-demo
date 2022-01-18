3e70247b09f8ca6cd572783036adb8c87d857bf5
yll <yangleilei@pwrd.com>
2022-01-18 17:30:38 +0800

    ✨ feat(app.js): cors 修改

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/index.html', function (req, res) {
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*'); //访问控制允许来源：所有
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); //访问控制允许报头 X-Requested-With: xhr请求
-  res.header('Access-Control-Allow-Metheds', 'PUT, POST, GET, DELETE, OPTIONS'); //访问控制允许方法
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS'); //访问控制允许方法
   res.header('X-Powered-By', 'nodejs'); //自定义头信息，表示服务端用nodejs
   res.header('Content-Type', 'application/json;charset=utf-8');
   next();
